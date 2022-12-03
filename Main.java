f9850f10450bfaccb04101f5bdcfd2be942fa9f1
Euros2308 <104604109+Euros2308@users.noreply.github.com>
2022-12-03 17:21:36 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("N =  ");
+        int N = in.nextInt();
+        int[][] matrix = new int[N][N];
+        int count = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0, value = i; j < matrix[i].length; j++, value += N) {
+                matrix[i][j] = count++;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
